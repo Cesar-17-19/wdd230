bab781c5862b88ef2628f27013203daf6c875a12
cesar-17-19 <aguilarrubiocesar@gmail.com>
2023-10-25 23:39:20 -0500

    bug fix commit

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -3,22 +3,24 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-    if (input.value != '') { }
+    if (input.value != '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+    }
 });
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-
-li.append(deleteButton);
-list.append(li);
-
-deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
-    input.focus();
-});
-
-input.focus()
-input.value = '';
